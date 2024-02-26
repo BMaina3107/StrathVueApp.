f56df641414088067c6e3678b77cb34a83b72a30
BMaina3107 <159436938+BMaina3107@users.noreply.github.com>
2024-02-26 19:06:19 +0300

    StrathVue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,13 @@
 <script>
 import Homepage from "./Homepage.vue"
 import Events from "./Events.vue"
-import Courses from "./Courses.vue"
 import ContactUs from "./ContactUs.vue"
 import AboutUs from "./AboutUs.vue"
+import Courses from "./Courses.vue"
 import Library from "./Library.vue"
 import Partnerships from "./Partnerships.vue"
 import Alumni from "./Alumni.vue"
+
 </script>
 
 <template>
@@ -14,26 +15,34 @@ import Alumni from "./Alumni.vue"
         <div>
             <v-btn to="/" color="white" dark >Home</v-btn>
          </div>
-        <div>
-         <v-btn to="/Courses" color="white" dark >Courses</v-btn>
-        </div>
+
+         <div>
+            <v-btn to="/Events" color="white" dark >Events</v-btn>
+         </div>
+
+         <div>
+            <v-btn to="/Courses" color="white" dark >Courses</v-btn>
+         </div>
+
+         <div>
+            <v-btn to="/Library" color="white" dark >Library</v-btn>
+         </div>
+
         <div>
             <v-btn to="/AboutUs" color="white" dark >AboutUs</v-btn>
         </div>
+        
         <div>
             <v-btn to="/ContactUs" color="white" dark >ContactUs</v-btn>
         </div>
-        <div>
-            <v-btn to="/Library" color="white" dark >Library</v-btn>
-        </div>
-        <div>
-            <v-btn to="/Partnerships" color="white" dark >Partnerships</v-btn>
-        </div>
+
         <div>
             <v-btn to="/Alumni" color="white" dark >Alumni</v-btn>
-        </div>
+         </div>
+
         <div>
-            <v-btn to="/Events" color="white" dark >Events</v-btn>
-        </div>
-    </v-app-bar>
+            <v-btn to="/Partnerships" color="white" dark >Partnerships</v-btn>
+         </div>
+       
+         </v-app-bar>
 </template>
